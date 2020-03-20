34a2838d04dc66ff9e7cad8d2e100f3452cb20d8
reynaldpn <reynaldpn@gmail.com>
2020-03-20 14:45:18 +0700

    fix links and add community link on read modal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 import { Route, Switch, Redirect } from 'react-router-dom'
 
-import Emoji from './components/Emoji'
-import ClassItem from './components/ClassItem'
-import ReadModal from './components/ReadModal'
-
-import API from './refs/API'
+import Home from './screens/Home'
+import ReadModal from './screens/ReadModal'
 
 export default class extends React.Component {
 	render() {
@@ -32,126 +29,5 @@ export default class extends React.Component {
 				</Switch>
 			</div>
 		)
-	}
-}
-
-class Home extends React.Component {
-	state = {
-		data: [],
-		selectedClassIndex: null
-	}
-
-	componentDidMount() {
-		this.loadData()
-	}
-
-	render() {
-		const { state } = this
-
-		const { data, selectedClassIndex } = state
-
-		return (
-			<>
-				<h1
-					className = 'app-title'
-				>
-					CrocoSchool
-				</h1>
-
-				<p
-					className = 'app-slogan'
-				>
-					Made with&nbsp;
-
-					<Emoji
-						label = '❤️'
-					/>
-
-					❤️selected skill share content media for everyone
-				</p>
-
-				{
-					data.map((item, index) => {
-						return (
-							<ClassItem
-								expandClassItemAtIndex = {() => this.expandClassItemAtIndex(index)}
-								index = {index}
-								item = {item}
-								isExpand = {index.toString() === (selectedClassIndex !== null ? selectedClassIndex.toString() : "")}
-								key = {item.class}
-								ref = {ref => this['classItem' + index.toString()] = ref}
-							/>
-						)
-					})
-				}
-
-				<a
-					className = 'telegram-content-container'
-					href = '/#'
-					onClick = {() => {
-						let otherWindow = window.open()
-
-						otherWindow.opener = null
-
-						otherWindow.location = 'https://t.me/crocodication'
-					}}
-				>
-					<img
-						alt = 'telegram logo'
-						height = {30}
-						src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png'
-						width = {30}
-						rel = "noopener noreferrer"
-					/>
-
-					<p
-						className = 'telegram-label'
-					>
-						Let's Discuss With Us On Telegram
-					</p>
-				</a>
-			</>
-		)
-	}
-
-	async expandClassItemAtIndex(index) {
-		let selectedClassIndex = await localStorage.getItem('selectedClassIndex')
-
-		if(selectedClassIndex !== index.toString()) {
-			selectedClassIndex = index
-
-			await localStorage.setItem('selectedClassIndex', index)
-		} else {
-			selectedClassIndex = null
-
-			await localStorage.removeItem('selectedClassIndex')
-		}
-		
-		this.setState({selectedClassIndex})
-	}
-
-	async loadData() {
-		let recentData = await localStorage.getItem('data')
-
-		const selectedClassIndex = await localStorage.getItem('selectedClassIndex')
-
-		if(recentData !== null) {
-			await this.setState({data: JSON.parse(recentData)})
-		}
-
-		await this.setState({selectedClassIndex})
-
-		if(this.state.data.length === 0) {
-			API.Content()
-			.then(res => res.json())
-			.then(async(resJson) => {
-				const data = resJson.data
-
-				await localStorage.setItem('data', JSON.stringify(data))
-
-				this.setState({data})
-			})
-			.catch(err => alert(err.toString()))
-		}
 	}
 }
---
+++ src/screens/ReadModal.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import { BASE_URL } from '../refs/API'
+
+export default class extends React.Component {
+    state = {
+        article: '',
+        lesson: null
+    }
+
+    async componentDidMount() {
+        this.loadArticle()
+    }
+
+    render() {
+        const { state } = this
+
+        const { article } = state
+
+        return  (
+            <>
+                <div
+                    className = 'read-modal-main-container'
+                >
+                    <Link
+                        to = '/#'
+                    >
+                        <div
+                            className = 'read-modal-background'
+                        />
+                    </Link>
+
+                    <div
+                        className = 'read-modal-content-container'
+                    >   
+                        <div
+                            className = 'read-modal-content'
+                            dangerouslySetInnerHTML = {{__html: article}}
+                        />
+
+                    <a
+                        className = 'telegram-content-container'
+                        href = '/#'
+                        onClick = {() => {
+                            let otherWindow = window.open()
+
+                            otherWindow.opener = null
+
+                            otherWindow.location = 'https://t.me/crocodication'
+                        }}
+                    >
+                        <img
+                            alt = 'telegram logo'
+                            height = {30}
+                            src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png'
+                            width = {30}
+                            rel = "noopener noreferrer"
+                        />
+
+                        <p
+                            className = 'telegram-label'
+                        >
+                            Let's Discuss With Us On Telegram
+                        </p>
+                    </a>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    async loadArticle() {
+        const id = window.location.href.split('/')[window.location.href.split('/').length - 1]
+
+        const recentArticlePage = await localStorage.getItem(id)
+
+        if(recentArticlePage !== null) {
+            await this.setState({article: recentArticlePage})
+        }
+        
+        fetch(BASE_URL + '/resources/html/' + id + '.html?loadtime=' + (new Date()).getTime().toString())
+        .then(res => res.text())
+        .then(async(resText) => {
+            await localStorage.setItem(id, resText)
+
+            this.setState({article: resText})
+        })
+    }
+}
