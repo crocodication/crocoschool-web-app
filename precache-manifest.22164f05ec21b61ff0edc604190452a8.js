c82a949261a7f34acee9e00cd2a71ab09da6b163
reynaldpn <reynaldpn@gmail.com>
2020-03-20 20:34:31 +0700

    Updates

---
+++ precache-manifest.22164f05ec21b61ff0edc604190452a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49eaa6684282027a297f5c0261cd3cd0",
+    "url": "/crocoschool/index.html"
+  },
+  {
+    "revision": "683ec1acd83086c14a09",
+    "url": "/crocoschool/static/css/main.75343831.chunk.css"
+  },
+  {
+    "revision": "fab7cd9f2cc793598a71",
+    "url": "/crocoschool/static/js/2.1c364b21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crocoschool/static/js/2.1c364b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683ec1acd83086c14a09",
+    "url": "/crocoschool/static/js/main.b6bf46a1.chunk.js"
+  },
+  {
+    "revision": "7042ccb825b94b4212cb",
+    "url": "/crocoschool/static/js/runtime-main.5291053c.js"
+  }
+]);
